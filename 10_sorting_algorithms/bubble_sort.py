1353df3ff9d83ccd20c0a9ad561559c3fcb852bf
Zhende-Liu <45912039+Zhende-Liu@users.noreply.github.com>
2019-02-25 21:21:12 +1100

    add the folder of 10 sorting algorithms

---
+++ 10_sorting_algorithms/bubble_sort.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019-02-25 21:18
+# @Author  : ZD Liu
+
---
+++ 10_sorting_algorithms/selection_sort.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019-02-25 21:19
+# @Author  : ZD Liu

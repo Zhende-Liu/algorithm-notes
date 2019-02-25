d7ac8787e2dffd742ccf2cfe34124fae31508496
Zhende-Liu <45912039+Zhende-Liu@users.noreply.github.com>
2019-02-25 22:45:33 +1100

    add bubble_sort.py & selection_sort.py

--- 10_sorting_algorithms/bubble_sort.py
+++ 10_sorting_algorithms/bubble_sort.py
@@ -3,3 +3,16 @@
 # @Time    : 2019-02-25 21:18
 # @Author  : ZD Liu
 
+def bubble_sort(obj_list):
+    for i in obj_list:
+        n = 0
+        while n + 1 < len(obj_list):
+            if obj_list[n] > obj_list[n+1]:
+                obj_list[n+1],obj_list[n] = obj_list[n],obj_list[n+1]
+            n += 1
+    return obj_list
+
+if __name__ == '__main__':
+    test_list = [2,5,4,2,3]
+    res = bubble_sort(test_list)
+    print(res)
--- 10_sorting_algorithms/selection_sort.py
+++ 10_sorting_algorithms/selection_sort.py
@@ -2,3 +2,23 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2019-02-25 21:19
 # @Author  : ZD Liu
+
+
+def selection_sort(obj_list):
+
+    for i in range(len(obj_list)):
+        min_ord = i
+        for v in range(len(obj_list[i:])):
+            if obj_list[min_ord] > obj_list[v]:
+                min_ord = v + i
+                print(min_ord)
+        obj_list[i],obj_list[min_ord] = obj_list[min_ord],obj_list[i]
+
+
+    return obj_list
+
+
+if __name__ == "__main__":
+    test_list = [2,5,4,2,3,4,8,9,7,6]
+    res = selection_sort(test_list)
+    print(res)
